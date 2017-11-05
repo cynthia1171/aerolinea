cd06f63271a8ee0f324280517067b8ea831a11ee
cynthia1171 <c.isla1171@gmail.com>
2017-11-05 18:02:23 -0300

    se agregan comentarios y se completa el js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,24 @@
 var asientoP = [true,true,true,true];
 var asientoE = [true,true,true,true,true,true];
 
+/* obtengo el id del boton reservar */
 var reservar = document.getElementById('reservar');
+
+/* obtengo las clases de los div que mostraran la disponibilidad de los asientos */
 var divP = document.querySelectorAll('.divP');
 var divE = document.querySelectorAll('.divE');
 
+/* obtengo el id del div que mostrara el pase de abordo */
+var contenedor = document.getElementById('cont');
+
+/* funcion para asignar asiento a primera clase */
 function primera(){
 	var cont = 0;
 	for (var i = 0; i < asientoP.length; i++) {
 		if (asientoP[i]===true) {
 			divP[i].classList.add('noDisp');
 			asientoP[i]=false;
+			contenedor.innerHTML = '<div><h3>Pase de abordar</h3><p>Numero de asiento: '+(i+1)+'</p><p>clase: Primera</p></div>';
 			break;
 		}else{
 			cont++;
@@ -28,12 +36,14 @@ function primera(){
 	}
 }
 
+/* funcion para asignar asiento a clase economica */
 function economica(){
 	var cont = 0;
 	for (var i = 0; i < asientoE.length; i++) {
 		if (asientoE[i]===true) {
 			divE[i].classList.add('noDisp');
 			asientoE[i]=false;
+			contenedor.innerHTML = '<div><h3>Pase de abordar</h3><p>Numero de asiento: '+(i+1)+'</p><p>clase: Economica</p></div>';
 			break;
 		}else{
 			cont++;
@@ -51,13 +61,17 @@ function economica(){
 	}
 }
 
+/* al presionar el boton reservar, se llama al evento click y se ejecuta la funcion */
 reservar.addEventListener('click', function(){
 	/* pregunto que clase desea y guardo la respuesta en la variable */
 	var reserva = prompt('¿En qué zona prefieres reservar tu asiento?\n 1. Primera Clase\n 2. Económica');
 
 	if (reserva === '1') {
-		primera();
+		primera();/* si la respuesta es 1, se ejecuta la funcion primera(). */
+		
+	}else if(reserva === '2'){
+		economica();/* si la respuesta es 2, se ejecuta la funcion economica(). */
 	}else{
-		economica();
+	 	alert("ingrese opcion valida");
 	}
 });
